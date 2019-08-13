78624f74c98a94701099aead9886be92608697ba
besseneger <dip.a@mail.ru>
2019-08-13 21:57:48 +0300

    refactoring_0 log_rotate

--- log_rotate/rotate_it.py
+++ log_rotate/rotate_it.py
@@ -4,19 +4,15 @@ import datetime
 
 files=["tst.log","HRS_RT_current.log"]
 logSize=1000000
+countOfFiles=5
 
 #функция переименовывает файл и конкатинирует дату с именем
 def rename_all(name):
     for file in files:
         if os.path.getsize(file) > logSize:
             t=datetime.datetime.today().strftime("%d%m%Y_%H%M%S")
-            end_index = file.find(".") # нужно найти индекс символа "."в имени файла. Это нужно чтобы использовать для переименования без расширения файла.
-            newFileName = file[0:end_index] # берётся диапазот индексов от 0 до символа "." и дальше эта переменная используется в переименовании.
             if os.path.isfile(file):
-                ren=os.rename(file, newFileName+"_"+t+"tst"+".arch")
-                createFile(file)
-                delOldFile(file)
-            else:
+                ren=os.rename(file, filePreparing(file)+"_"+t+".arch")
                 createFile(file)
                 delOldFile(file)
         else:
@@ -26,14 +22,17 @@ def createFile(trueFileName):
     f=open(trueFileName, "w+")
     f.close
 
+def filePreparing(fileForPrep):
+    end_index = fileForPrep.find(".") # нужно найти индекс символа "."в имени файла. Это нужно чтобы использовать для переименования без расширения файла.
+    newFileName = fileForPrep[0:end_index] # берётся диапазот индексов от 0 до символа "." и дальше эта переменная используется в переименовании.
+    return(newFileName)
+
 def delOldFile(name):
-    end_index = name.find(".")
-    newFileName = name[0:end_index]
     list=[]
-    for itfile in glob.glob(newFileName+"_*"):
+    for itfile in glob.glob(filePreparing(name)+"_*"):
         list.append(itfile)
     sorted(list)
-    while len(list) > 5:
+    while len(list) > countOfFiles:
         os.remove(list[0])
         print("Удаляется файл "+list[0])
         del list[0]
